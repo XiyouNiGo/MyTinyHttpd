11c1a12c5f6920707f7b97c49c2be9b41e34bd0b
XiyouNiGo <1275810355@qq.com>
2021-10-30 23:03:35 +0800

    feat: add atomic class

---
+++ mytinyhttpd/base/atomic.h
@@ -0,0 +1,51 @@
+#ifndef MYTINYHTTPD_BASE_ATOMIC_H_
+#define MYTINYHTTPD_BASE_ATOMIC_H_
+
+#include <stdint.h>
+
+#include "mytinyhttpd/utils/noncopyable.h"
+
+namespace mytinyhttpd {
+
+template <typename T>
+class Atomic : noncopyable {
+ public:
+  Atomic() : value_(0) {}
+
+  T Load() { return __sync_val_compare_and_swap(&value_, 0, 0); }
+
+  void Store(T x) { return LoadStore(x); }
+
+  T FetchAdd(T x) { return __sync_fetch_and_add(&value_, x); }
+
+  T FetchSub(T x) { return __sync_fetch_and_sub(&value_, x); }
+
+  T FetchAnd(T x) { return __sync_fetch_and_and(&value_, x); }
+
+  T FetchOr(T x) { return __sync_fetch_and_or(&value_, x); }
+
+  T FetchXor(T x) { return __sync_fetch_and_xor(&value_, x); }
+
+  T AddFetch(T x) { return FetchAdd(x) + x; }
+
+  T SubFetch(T x) { return FetchSub(x) - x; }
+
+  T operator++() { return AddFetch(1); }
+
+  T operator--() { return SubFetch(1); }
+
+  void Add(T x) { FetchAdd(x); }
+
+  void Sub(T x) { FetchSub(x); }
+
+  T LoadStore(T x) {
+    return __sync_lock_test_and_set(&value_, x);
+  }
+
+ private:
+  volatile T value_;
+};
+
+}  // namespace mytinyhttpd
+
+#endif  // !MYTINYHTTPD_BASE_ATOMIC_H_
