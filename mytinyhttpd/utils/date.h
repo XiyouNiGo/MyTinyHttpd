74fb2b563b858c99c379776d965d83bfd63f3ce2
XiyouNiGo <1275810355@qq.com>
2021-11-03 11:42:08 +0800

    base: add date class

---
+++ mytinyhttpd/utils/date.h
@@ -0,0 +1,67 @@
+#ifndef MYTINYHTTPD_UTILS_DATE_H_
+#define MYTINYHTTPD_UTILS_DATE_H_
+
+#include <boost/operators.hpp>
+#include <string>
+
+#include "mytinyhttpd/utils/copyable.h"
+
+namespace mytinyhttpd {
+
+// convert year month day and julianDay
+class Date : public mytinyhttpd::copyable,
+             public boost::less_than_comparable<Date>,
+             public boost::equality_comparable<Date> {
+ public:
+  struct YearMonthDay {
+    int year;
+    int month;
+    int day;
+  };
+
+  static const int kDaysPerWeek = 7;
+  static const int kJulianDayOf1970_01_01;
+
+  Date() : julian_day_number_(0) {}
+
+  Date(int year, int month, int day);
+
+  explicit Date(int julian_day_num) : julian_day_number_(julian_day_num) {}
+
+  explicit Date(const struct tm&);
+
+  void swap(Date& date) {
+    std::swap(julian_day_number_, date.julian_day_number_);
+  }
+
+  bool Valid() const { return julian_day_number_ > 0; }
+
+  std::string ToIsoString() const;
+
+  struct YearMonthDay ToYearMonthDay() const;
+
+  int GetYear() const { return ToYearMonthDay().year; }
+
+  int GetMonth() const { return ToYearMonthDay().month; }
+
+  int GetDay() const { return ToYearMonthDay().day; }
+
+  int ToWeekDay() const { return (julian_day_number_ + 1) % kDaysPerWeek; }
+
+  int GetJulianDayNumber() const { return julian_day_number_; }
+
+ private:
+  int julian_day_number_;
+};
+
+inline bool operator<(Date x, Date y) {
+  return x.GetJulianDayNumber() < y.GetJulianDayNumber();
+}
+
+inline bool operator==(Date x, Date y) {
+  return x.GetJulianDayNumber() == y.GetJulianDayNumber();
+}
+
+}  // namespace mytinyhttpd
+
+#endif  // !MYTINYHTTPD_UTILS_DATE_H_
