2f25d0b8d1c3e8d8319c266d273cd7a4b769ecab
XiyouNiGo <1275810355@qq.com>
2021-11-10 16:57:07 +0800

    base: add Thread class

--- mytinyhttpd/base/thread.h
+++ mytinyhttpd/base/thread.h
@@ -8,13 +8,51 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <functional>
+#include <memory>
+
+#include "mytinyhttpd/base/atomic.h"
+#include "mytinyhttpd/base/count_down_latch.h"
+#include "mytinyhttpd/utils/noncopyable.h"
+
 namespace mytinyhttpd {
 
 namespace detail {
 
-inline pid_t gettid() { return static_cast<pid_t>(::syscall(SYS_gettid)); }
+inline pid_t gettid() { return static_cast<pid_t>(syscall(SYS_gettid)); }
 
 }  // namespace detail
+
+class CountDownLatch;
+
+class Thread : noncopyable {
+ public:
+  typedef std::function<void()> ThreadFunc;
+
+  explicit Thread(ThreadFunc, const std::string& name = std::string());
+  ~Thread();
+
+  void Start();
+  bool IsStarted() const { return started_; }
+
+  int Join();
+
+  pid_t tid() const { return tid_; }
+  const std::string& name() const { return name_; }
+  static int num_created() { return num_created_.Load(); }
+
+ private:
+  void SetDefaultName();
+
+  bool started_;
+  bool joined_;
+  pthread_t pthread_id_;  // only exposed to Pthread Library
+  pid_t tid_;
+  ThreadFunc func_;
+  std::string name_;
+  CountDownLatch latch_;
+  static Atomic<int32_t> num_created_;
+};
 
 }  // namespace mytinyhttpd
 
