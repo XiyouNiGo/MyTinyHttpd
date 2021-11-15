e674e48a056401d32215c8bca708dbfdcc44e76c
XiyouNiGo <1275810355@qq.com>
2021-11-15 09:56:03 +0800

    net: add typedef for callbacks

---
+++ mytinyhttpd/net/callbacks.h
@@ -0,0 +1,34 @@
+#ifndef MYTINYHTTPD_NET_CALLBACKS_H_
+#define MYTINYHTTPD_NET_CALLBACKS_H_
+
+#include <functional>
+#include <memory>
+
+#include "mytinyhttpd/utils/timestamp.h"
+
+namespace mytinyhttpd {
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
+
+namespace net {
+
+class Buffer;
+class TcpConnection;
+
+typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
+typedef std::function<void()> TimerCallback;
+typedef std::function<void(const TcpConnectionPtr&)> ConnectionCallback;
+typedef std::function<void(const TcpConnectionPtr&)> CloseCallback;
+typedef std::function<void(const TcpConnectionPtr&)> WriteCompleteCallback;
+typedef std::function<void(const TcpConnectionPtr&, size_t)>
+    HighWaterMarkCallback;
+typedef std::function<void(const TcpConnectionPtr&, Buffer*, Timestamp)>
+    MessageCallback;
+
+}  // namespace net
+
+}  // namespace mytinyhttpd
+
+#endif  // !MYTINYHTTPD_NET_CALLBACKS_H_
